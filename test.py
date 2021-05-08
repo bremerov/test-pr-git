0cf112f5e6c12c39ad38297598a4922a6cc2ecff
Rene Bremer <rebremer@microsoft.com>
2021-05-08 17:46:08 +0200

    add power

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ def productNumbers(num1,num2):
 def divideNumbers(num1,num2):
     return num1 / num2
 
+def powerNumbers(num1,num2):
+    return pow(num1, num2)
+
 som1=addNumbers(1,4)
 som2=substractNumbers(1,4)
 print(str(som1 + som2))
@@ -17,3 +20,6 @@ print(str(som1 + som2))
 divide=divideNumbers(1,4)
 product=productNumbers(1,4)
 print(str(divide + product))
+
+pow=powerNumbers(1,4)
+print(str(pow))
