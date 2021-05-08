a1a3101203fe1b02355b461c05ca056842e3259e
Rene Bremer <rebremer@microsoft.com>
2021-05-08 14:51:45 +0200

    add divide

--- test.py
+++ test.py
@@ -7,10 +7,13 @@ def substractNumbers(num1,num2):
 def productNumbers(num1,num2):
     return num1 * num2
 
+def divideNumbers(num1,num2):
+    return num1 / num2
+
 som1=addNumbers(1,4)
 som2=substractNumbers(1,4)
 print(str(som1 + som2))
 
-som=addNumbers(1,4)
+divide=divideNumbers(1,4)
 product=productNumbers(1,4)
-print(str(som + product))
+print(str(divide + product))
