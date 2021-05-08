3641a87355c60fec0b8009e8cb378bd385ab02c1
Rene Bremer <rebremer@microsoft.com>
2021-05-08 14:22:35 +0200

    add substract to python

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 def addNumbers(num1,num2):
     return num1 + num2
 
-som=addNumbers(1,4)
-print(str(som))
+def substractNumbers(num1,num2):
+    return num1 - num2
+
+som1=addNumbers(1,4)
+som2=substractNumbers(1,4)
+print(str(som1 + som2))
